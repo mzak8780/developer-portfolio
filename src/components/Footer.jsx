fd7c2a975d791cf61a9972941990accb04909e39
mzak8780 <maks.zak@hotmail.com>
2025-01-16 16:26:02 +1100

    feat/add-social-media-links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="py-5 text-center">
-      {/* {Social Icons}  */}
+      <div className="flex justify-center space-x-4">
+        <a
+          href="https://www.linkedin.com/in/maks-zak/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-gray-900"
+        >
+          <FaLinkedin size={24} />
+        </a>
+        <a
+          href="https://github.com/mzak8780"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-gray-900"
+        >
+          <FaGithub size={24} />
+        </a>
+      </div>
       <p className="text-sm mt-2 opacity-50">
         &copy; {new Date().getFullYear()} Maksymilian Zak. All rights reserved.
       </p>
