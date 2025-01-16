0c747d8c0415d4b701de9c889638e4fd52c89c0c
mzak8780 <maks.zak@hotmail.com>
2025-01-16 15:14:42 +1100

    fix/config-syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{jsx,js}"],
@@ -12,7 +15,7 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
   daisyui: {
     themes: [
       "light",
