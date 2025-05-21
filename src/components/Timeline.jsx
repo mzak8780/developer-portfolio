b2de5ae853d9403051644c1de8c2a10fc46e8f64
mzak8780 <maks.zak@hotmail.com>
2025-05-21 20:15:43 +1000

    feat/update-resume

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,40 +4,40 @@ import TimelineItem from "./TimelineItem";
 import Title from "./Title";
 
 function Timeline() {
-  return (
-    <div className="flex flex-col md:flex-row justify-center items-center my-10 px-4">
-      <div className="w-full md:w-1/4 flex justify-center items-center mb-10 md:mb-0 ">
-        <img
-          src="assets/graphic.jpg"
-          alt="WFH Illustration"
-          className="w-1/3 md:w-full"
-        />
-      </div>
-      <div className="w-full md:w-3/4">
-        <Title id="timeline-title">Timeline</Title>
-        {timeline.map((project) => (
-          <TimelineItem
-            key={project.title}
-            year={project.year}
-            title={project.title}
-            company={project.company}
-            duration={project.duration}
-            details={project.details}
-          />
-        ))}
-        <div className="flex justify-center mt-10">
-          <a
-            href="MaksymilianZak_25.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-wide bg-base-content text-center inline-block px-8 py-3 w-max text-base text-white font-medium rounded-md bg-gradient-to-r drop-shadow-md"
-          >
-            Download Full Resume
-          </a>
+    return (
+        <div className="flex flex-col md:flex-row justify-center items-center my-10 px-4">
+            <div className="w-full md:w-1/4 flex justify-center items-center mb-10 md:mb-0 ">
+                <img
+                    src="assets/graphic.jpg"
+                    alt="WFH Illustration"
+                    className="w-1/3 md:w-full"
+                />
+            </div>
+            <div className="w-full md:w-3/4">
+                <Title id="timeline-title">Timeline</Title>
+                {timeline.map((project) => (
+                    <TimelineItem
+                        key={project.title}
+                        year={project.year}
+                        title={project.title}
+                        company={project.company}
+                        duration={project.duration}
+                        details={project.details}
+                    />
+                ))}
+                <div className="flex justify-center mt-10">
+                    <a
+                        href="MaksZak.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-wide bg-base-content text-center inline-block px-8 py-3 w-max text-base text-white font-medium rounded-md bg-gradient-to-r drop-shadow-md"
+                    >
+                        Download Full Resume
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Timeline;
