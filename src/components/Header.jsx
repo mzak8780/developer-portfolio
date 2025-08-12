eb4bdad4421dc7c56c8804ffc49f53d90e6a66b7
mzak8780 <maks.zak@hotmail.com>
2025-08-12 18:46:17 +1000

    feat/update-image-path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
         <header className="absolute flex items-center justify-left top-4 left-7 z-10 w-full">
             <div className="container w-full flex h-12 items-center">
                 <img
-                    src="public\assets\green_planet.png"
-                    alt="Space Dude on Rocket"
+                    src="/assets/green_planet.png"
+                    alt="Green Planet"
                     className="h-12 w-12 mr-2"
                 ></img>
                 <div className="font-space-grotesk font-bold">
